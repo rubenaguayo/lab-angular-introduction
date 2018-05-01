ddff6961ed7e1c7d77eea27a19571fbd8917e346
Ruben Aguayo <Pelao@MacBook-Pro-de-Ruben.local>
2018-05-01 09:33:36 +0200

    BCN-RUBÉN

---
+++ starter-code/iteration2/iteration2/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Learn how to code';
+  image = 'https://www.google.es/search?q=ironhack&source=lnms&tbm=isch&sa=X&ved=2ahUKEwietImuweLaAhVDYpoKHZ5BDBYQ_AUoA3oECAAQBQ&biw=1280&bih=627#imgrc=hXLo6wSX6amBpM:';
+  description = 'ironhack';
+}
